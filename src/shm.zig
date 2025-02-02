d3d50058df21f813a0482f5e7d7b95c58e24670d
kaup <kalebroyolson+github@gmail.com>
2025-02-02 14:41:10 -0600

    wip os_create_anonymous_file

--- src/shm.zig
+++ src/shm.zig
@@ -1,37 +1,49 @@
 const std = @import("std");
 const posix = std.posix;
-const mman = @cImport({
-    @cInclude("sys/mman.h");
-});
-const errno = @cImport({
-    @cInclude("errno.h");
-});
+const os = std.os;
 const cstd = @cImport({
     @cInclude("stdlib.h");
 });
 
-fn set_cloexec_or_close(fd: usize) usize {
-    var flags: i64 = 0;
+const ShmError = error{
+    Enoent,
+    NoFd,
+    NoFlags,
+};
 
+fn set_cloexec_or_close(fd: usize) ShmError!usize {
     if (fd == -1) {
-        return -1;
+        return ShmError.NoFd;
     }
 
-    flags = posix.fcntl(fd, posix.F.GETFD, 0);
+    const flags = posix.fcntl(fd, posix.F.GETFD, 0);
     if (flags == -1 || (posix.fcntl(fd, posix.F.SETFD, flags | posix.FD_CLOEXEC) == -1)) {
         posix.close(fd);
-        return -1;
+        return ShmError.NoFlags;
     }
 
     return fd;
 }
 
-fn create_tmpfile_cloexec(tmpname: *u8) usize {
-    var fd: usize = 0;
-
-    fd = cstd.mkstemp(tmpname);
+fn create_tmpfile_cloexec(tmpname: *u8) !usize {
+    const fd_c: c_int = cstd.mkstemp(tmpname);
+    var fd: usize = @intCast(fd_c);
     if (fd >= 0) {
         fd = set_cloexec_or_close(fd);
         posix.unlink(tmpname);
     }
+}
+
+fn os_create_anonymous_file(size: u64) ShmError!usize {
+    const template: []u8 = "/wlblocks-shared-XXXXXX";
+    const name: *u8 = null;
+    var fd: usize = 0;
+
+    const path: []u8 = posix.getenv("XDG_RUNTIME_DIR");
+    if (path == null) {
+        return ShmError.Enoent;
+    }
+
+
+
 }
