c5ed292c540919782c2ebc745f5814ec429627a7
kaup <kalebroyolson+github@gmail.com>
2025-02-11 18:19:32 -0600

    someday it will compile again

--- src/shm.zig
+++ src/shm.zig
@@ -36,8 +36,8 @@ fn set_cloexec_or_close(fd: usize) ShmError!usize {
     return fd;
 }
 
-fn create_tmpfile_cloexec(tmpname: *u8) !i32 {
-    const fd_c: c_int = cstd.mkstemp(tmpname);
+fn create_tmpfile_cloexec(tmpname: *[]u8) !i32 {
+    const fd_c: c_int = cstd.mkstemp(@ptrCast(tmpname));
     var fd: i32 = @intCast(fd_c);
     if (fd >= 0) {
         fd = set_cloexec_or_close(fd);
@@ -48,11 +48,11 @@ fn create_tmpfile_cloexec(tmpname: *u8) !i32 {
 fn os_create_anonymous_file(size: u64) CreateAnonymousFileError!i32 {
     const template = "/wlblocks-shared-XXXXXX";
 
-    const path = try get_runtime_dir();
-    const buf: [path.len + template.len]u8 = undefined;
-    const name = try std.fmt.bufPrint(&buf, "{s}{s}", .{ path, template });
+    var path_buf: [std.fs.max_path_bytes]u8 = undefined;
+    const xdg_runtime_dir = try get_runtime_dir();
+    var name = try std.fmt.bufPrint(&path_buf, "{s}{s}", .{ xdg_runtime_dir, template });
 
-    const fd = try create_tmpfile_cloexec(name);
+    const fd = try create_tmpfile_cloexec(&name);
     if (fd == -1) {
         return ShmError.NoFd;
     }
@@ -65,30 +65,34 @@ fn os_create_anonymous_file(size: u64) CreateAnonymousFileError!i32 {
     return fd;
 }
 
-fn get_runtime_dir() ShmError![]u8 {
-    const path: []u8 = posix.getenv("XDG_RUNTIME_DIR") orelse {
+fn get_runtime_dir() ShmError![:0]const u8 {
+    const path = posix.getenvZ("XDG_RUNTIME_DIR") orelse {
         return ShmError.Enoent;
     };
     return path;
 }
 
-pub fn create_buffer(width: usize, height: usize) CreateBufferError!*wl.wl_buffer {
+pub fn create_buffer(width: i32, height: i32) CreateBufferError!*wl.wl_buffer {
     const stride = width * 4;
     const size = stride * height;
 
-    const fd = try os_create_anonymous_file(size);
+    const fd = try os_create_anonymous_file(@intCast(size));
     const sharedmap: std.os.linux.MAP = .{ .TYPE = std.os.linux.MAP_TYPE.SHARED };
 
-    const mmap_result = posix.mmap(null, size, posix.PROT.READ | posix.PROT.WRITE, sharedmap, @intCast(fd), 0) catch |err| {
+    const mmap_result = posix.mmap(null, @intCast(size), posix.PROT.READ | posix.PROT.WRITE, sharedmap, @intCast(fd), 0) catch |err| {
         io.print("Failed to mmap\n", .{});
         posix.close(fd);
         return err;
     };
     shm_data = @ptrCast(mmap_result);
 
-    const pool: *wl.wl_shm_pool = wl.wl_shm_create_pool(shm, fd, @intCast(size));
-    const buff: *wl.wl_buffer = wl.wl_shm_pool_create_buffer(pool, 0, width, height, stride, wl.WL_SHM_FORMAT_ARGB8888);
+    const pool: ?*wl.wl_shm_pool = wl.wl_shm_create_pool(shm, fd, @intCast(size));
+    const buff: ?*wl.wl_buffer = wl.wl_shm_pool_create_buffer(pool, 0, width, height, stride, wl.WL_SHM_FORMAT_ARGB8888);
     wl.wl_shm_pool_destroy(pool);
 
-    return buff;
+    if (buff == null) {
+        return ShmError.Enoent;
+    } else {
+        return buff.?;
+    }
 }
